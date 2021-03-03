4dc0914339073712827dad7956d9dea4758189ea
Khoroshavin <khoroshavin.russia@gmail.com>
2021-03-03 17:21:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <search-input @gifs-fetched="onGifsFetched"/>
+
+    <gif-list :gifs="gifs"/>
+</template>
+
+<script>
+import SearchInput from "@/components/Search";
+import GifList from "@/components/GifList";
+export default {
+  name: 'App',
+  components: {
+    GifList,
+    SearchInput
+  },
+  data() {
+    return {
+      gifs: [],
+    }
+  },
+  methods: {
+    onGifsFetched(result) {
+      console.log(result);
+      this.gifs = result.data
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  padding: 50px
+}
+#app {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
